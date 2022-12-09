6cfb4774a4a669569869cae1ec75d6d891f6d89d
JonKnitt1205 <jonknitt1205@gmail.com>
2022-12-08 23:39:25 -0500

    finished video 5 of cherno's opengl series

--- MyEngine/src/Application.cpp
+++ MyEngine/src/Application.cpp
@@ -1,4 +1,8 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
@@ -19,17 +23,38 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize GLEW */
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW failed to init" << std::endl;
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    /* Creating our vertex buffer for our triangle */
+    float positions[] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+
+    /* Giving this vertex to the gpu and giving it a buffer id */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    
+    /* Telling the layout of the buffer */
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+    glEnableVertexAttribArray(0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        /* Draw the triangle */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
